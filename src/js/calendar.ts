2ced4ea7c512c5cbd6198bcbd4d16a6ae50d495e
Dobrota <dobrota.art@gmail.com>
2024-02-01 20:57:20 +0200

    update

---
+++ src/js/calendar.ts
@@ -0,0 +1,36 @@
+import VanillaCalendar, { Options } from 'vanilla-calendar-pro';
+import 'vanilla-calendar-pro/build/vanilla-calendar.min.css';
+
+const options: Options = {
+  input: true,
+  type: 'multiple',
+  settings: {
+    range: {
+      disablePast: true,
+    },
+    selection: {
+      day: 'multiple-ranged',
+    },
+    visibility: {
+      daysOutside: false,
+    },
+  },
+  actions: {
+    changeToInput(e, calendar, self) {
+      if (!self.HTMLInputElement) return;
+      if (self.selectedDates[1]) {
+        self.selectedDates.sort((a, b) => +new Date(a) - +new Date(b));
+        self.HTMLInputElement.value = `${self.selectedDates[0]} — ${
+          self.selectedDates[self.selectedDates.length - 1]
+        }`;
+      } else if (self.selectedDates[0]) {
+        self.HTMLInputElement.value = self.selectedDates[0];
+      } else {
+        self.HTMLInputElement.value = '';
+      }
+    },
+  },
+};
+
+const calendarInput = new VanillaCalendar('#calendar-input', options);
+calendarInput.init();
